9cb68fe04a9d87dfb1e45a094584a96dbef8c178
Sandeep Sawhney <ssandeepsawhney@gmail.com>
2026-04-12 00:01:40 -0400

    Downgrade recharts to v2 to fix TDZ circular dependency error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      recharts: path.resolve(__dirname, 'node_modules/recharts/es6/index.js'),
-    },
-  },
 })
